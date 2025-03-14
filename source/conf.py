6b1c83420d3f336e1b8d71426a8de62cfa73d1e1
004Ajay <ajaytshaju@gmail.com>
2025-03-14 21:26:49 +0530

    new theme for UC&C

---
+++ source/conf.py
@@ -0,0 +1,34 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'UC&C'
+copyright = 'Ajay, 2025'
+author = 'Ajay'
+release = '1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+# Extensions installed using pip, pip install sphinx-copybutton
+
+extensions = [
+    'sphinx_copybutton',
+]
+
+copybutton_exclude_patterns = ">>> |\$"
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'nature'
+html_static_path = ['_static']
