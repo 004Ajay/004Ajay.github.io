b8c40d32878f13efaafc0a9af850a40575cd238d
004Ajay <ajaytshaju@gmail.com>
2025-03-14 22:36:04 +0530

    mytest2

--- UC&C/source/conf.py
+++ UC&C/source/conf.py
@@ -30,6 +30,6 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+html_baseurl = "https://004ajay.github.io/UC&C/build/html/_static"
 html_theme = 'nature'
 html_static_path = ['_static']
-html_baseurl = "https://004ajay.github.io/UC&C/source/_static"
