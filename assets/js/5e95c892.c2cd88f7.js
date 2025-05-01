01144fba860416767c11f8fa22c3d9c5c475df1a
004Ajay <ajaytshaju@gmail.com>
2025-05-01 11:08:53 +0530

    partial website

---
+++ assets/js/5e95c892.c2cd88f7.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebsite_builder=self.webpackChunkwebsite_builder||[]).push([[9647],{8122:(e,s,r)=>{r.r(s),r.d(s,{default:()=>d});r(6540);var u=r(4164),t=r(1082),a=r(204),c=r(2831),i=r(1410),l=r(4848);function d(e){return(0,l.jsx)(t.e3,{className:(0,u.A)(a.G.wrapper.docsPages),children:(0,l.jsx)(i.A,{children:(0,c.v)(e.route.routes)})})}}}]);
